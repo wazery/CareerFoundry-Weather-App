68c2204b30452a275927bdd24808be8c7886c937
Islam Wazery <wazery@ubuntu.com>
2017-09-21 22:53:06 +0200

    Add a view helper method for getting city map thumb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
   def title(value)
     @title = "#{value} | CareerfoundryWeather" unless value.nil?
   end
+
+  def gmap_home_thumb(options = {})
+    geolocation = options[:geolocation] || '52.520199,%2013.409859'
+    size        = options[:size] || '256x256'
+    zoom        = options[:zoom] || '12'
+
+    "https://maps.googleapis.com/maps/api/staticmap?center=#{geolocation}&zoom=#{zoom}&size=#{size}"
+  end
 end
